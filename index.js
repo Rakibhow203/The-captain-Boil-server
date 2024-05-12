ddde6554371f99978662933aedc9317389cd114c
rakibhow203 <rakibhossainhow65@gmail.com>
2024-05-13 01:15:39 +0600

    added allfoods section

--- index.js
+++ index.js
@@ -29,14 +29,28 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const allFoodCollection =client.db('TheCaptainBoil').collection('AllFoods')
+ app.get('/foods', async (req, res) => {
+      const cursor = allFoodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+ });
+    
+     const topFoodCollection =client.db('TopFoods').collection('TopFood')
+     app.get('/tops', async (req, res) => {
+      const cursor = allFoodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
